a7934cb95d2b01aa41463aab239516043fb773ac
ShaeChristmas <shainechristmas@gmail.com>
2020-07-13 16:22:31 +1000

    ADD: Add forEach function to display each device in table.

--- app.js
+++ app.js
@@ -3,3 +3,21 @@ const devices = [];
 devices.push({user: "Shae", name: "Shae's Laptop"});
 devices.push({ user: "Mary", name: "Mary's iPhone" });
 devices.push({ user: "Shae", name: "Shae's Pixel 3" });
+
+devices.forEach(function(device) {
+    const table = document.querySelector('#devices');
+    const row = document.createElement('tr');
+
+    const user = document.createElement('td');
+    const userText = document.createTextNode(device.user);
+    user.appendChild(userText);
+
+    const name = document.createElement('td');
+    const nameText = document.createTextNode(device.name);
+    name.appendChild(nameText);
+
+    row.appendChild(user);
+    row.appendChild(name);
+
+    table.appendChild(row);
+});
