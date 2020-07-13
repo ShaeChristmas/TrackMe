1d34e3e6f4a40e810346c98e235c678ba391019c
ShaeChristmas <shainechristmas@gmail.com>
2020-07-13 16:13:36 +1000

    OTHER: Populate data in devices

---
+++ app.js
@@ -0,0 +1,5 @@
+const devices = [];
+
+devices.push({user: "Shae", name: "Shae's Laptop"});
+devices.push({ user: "Mary", name: "Mary's iPhone" });
+devices.push({ user: "Shae", name: "Shae's Pixel 3" });
