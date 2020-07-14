3fdaa8e502181f4b539e22005cfc40d89a52eae6
ShaeChristmas <shainechristmas@gmail.com>
2020-07-14 18:52:56 +1000

    ADD: Add base functionality for navbar

--- web/public/app.js
+++ web/public/app.js
@@ -1,6 +1,8 @@
 //Init device list
 const devices = JSON.parse(localStorage.getItem('devices')) || [];
 
+$('#navbar').load('navbar.html');
+
 //devices.push({user: "Shae", name: "Shae's Laptop"});
 //devices.push({ user: "Mary", name: "Mary's iPhone" });
 //devices.push({ user: "Shae", name: "Shae's Pixel 3" });
