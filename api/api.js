4a867330157d2597feaae4b9f4e4d3d80c613035
ShaeChristmas <shainechristmas@gmail.com>
2020-07-23 01:46:50 +1000

    ADD: Add models folder with device.js

--- api/api.js
+++ api/api.js
@@ -1,5 +1,8 @@
 const express = require('express');
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://schristmas:paperSw4n@cluster0.7g4gx.mongodb.net/TrackMe',{useNewUrlParser: true, useUnifiedTopology: true});
+
 const app = express();
 const port = 5000;
 
